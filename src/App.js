58f10d9dd150833ad1a9647c88881a9dd3815e75
simoncarriere <simoncarriere@icloud.com>
2021-04-05 11:27:50 +0545

    Use Local Storage to store Todos

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
-import React, {useState, useRef} from 'react'
+import React, {useState, useRef, useEffect} from 'react'
 import TodoList from './TodoList'
+import uuidv4 from 'uuid/v4'
 
+
+const LOCAL_STORAGE_KEY = 'todoApp.todos'
 
 function App() {
 
   const [todos, setTodos] = useState([ ])
   const todoNameRef = useRef()
 
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+
+    if (storedTodos) setTodos(storedTodos)
+  },[])
+
+  // Local Storage Update and Store
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  },[todos])
+
+
   function handleAddTodo(e){
     const name = todoNameRef.current.value
     if (name === '') return 
     setTodos(prevTodos => {
-      return[...prevTodos, {id: 1, name:name, complete:false}]
+      return[...prevTodos, {id: uuidv4(), name:name, complete:false}]
     })
     todoNameRef.current.value = null
   }
